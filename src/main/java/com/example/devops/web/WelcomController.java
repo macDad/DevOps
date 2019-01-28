317394f3f367d0d2a6a688fe2b55f2df234f9cc2
CORE-LT016\Administrator <jaya.maduka@gmail.com>
2019-01-29 00:24:26 +0800

    Updated with SCM and dist repo

---
+++ src/main/java/com/example/devops/web/WelcomController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.example.devops.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Administrator
+ *
+ */
+@Controller
+public class WelcomController {
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("course", "DevOps");
+		return "index";
+	}
+}
